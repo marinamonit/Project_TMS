e78f5bdedc7684f0bb260c0550d70800905305a2
Marina <marinamonit1990@example.com>
2024-05-17 16:55:01 +0300

    First commit

---
+++ main/views.py
@@ -0,0 +1,35 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    context = {"title": "Home - Главная", "content": "Магазин мебели HOME"}
+
+    return render(request, "main/index.html", context)
+
+
+def about(request):
+    context = {
+        "title": "Home - О нас",
+        "content": "О нас",
+        "text_on_page": "Текст о магазине",
+    }
+    return render(request, "main/about.html", context)
+
+
+def dostavka(request):
+    context = {
+        "title": "Доставка",
+        "content": "Доставка и оплата",
+        "text_on_page": "Текст о доставке и оплате",
+    }
+    return render(request, "main/dostavka.html", context)
+
+
+def contacts(request):
+    context = {
+        "title": "Контакты",
+        "content": "Наши контакты",
+        "text_on_page": "Данные контактов",
+    }
+    return render(request, "main/contacts.html", context)
