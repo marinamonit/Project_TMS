8ce4008bb28882f8b4f589b3ee8d5e690fe06f5b
Marina <marinamonit1990@example.com>
2024-05-24 14:03:56 +0300

    register simple tag

--- app/urls.py
+++ app/urls.py
@@ -18,8 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from app.settings import DEBUG
-
+from app import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -33,7 +33,8 @@ urlpatterns = [
     ),
 ]
 
-if DEBUG:
+if settings.DEBUG:
     urlpatterns += [
         path("__debug__/", include("debug_toolbar.urls")),
     ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
